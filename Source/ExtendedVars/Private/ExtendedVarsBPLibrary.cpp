18671739ee270763f7c2104dc881f9d470a4a3f7
Frozen Forest Reality Technologies <frozenforeststudio@gmail.com>
2023-05-25 02:10:27 +0300

    V2
    New Render functions and more organized names, categories

--- Source/ExtendedVars/Private/ExtendedVarsBPLibrary.cpp
+++ Source/ExtendedVars/Private/ExtendedVarsBPLibrary.cpp
@@ -11,7 +11,8 @@
 #include "Kismet/KismetMathLibrary.h"
 #include "Misc/Base64.h"                        // Encode / Decode Base64 
 #include "Misc/FileHelper.h"                    // Load File to Array
-#include "HAL/FileManager.h"                    // Save Bitmap to Disk
+#include "HAL/FileManager.h"                    // Save Texture as Bitmap
+#include "ImageUtils.h"                         // Save Texture as Jpeg
 
 UExtendedVarsBPLibrary::UExtendedVarsBPLibrary(const FObjectInitializer& ObjectInitializer)
 : Super(ObjectInitializer)
@@ -19,34 +20,30 @@ UExtendedVarsBPLibrary::UExtendedVarsBPLibrary(const FObjectInitializer& ObjectI
 
 }
 
-// Bytes Group
+// Path Group.
 
-FString UExtendedVarsBPLibrary::BytesArrayToString(TArray<uint8> In_Bytes)
+FString UExtendedVarsBPLibrary::Android_Path_Helper(FString In_FileName)
 {
-    if (In_Bytes.Num() > 0)
+    if (UGameplayStatics::GetPlatformName() == "Android" || UGameplayStatics::GetPlatformName() == "IOS")
     {
-        const std::string Body_String(reinterpret_cast<const char*>(In_Bytes.GetData()), In_Bytes.Num());
-        FString Body_FString = Body_String.c_str();
+        if (In_FileName.IsEmpty() == true)
+        {
+            return "";
+        }
 
-        return Body_FString;
+        FString Path_Referance = FPlatformFileManager::Get().GetPlatformFile().ConvertToAbsolutePathForExternalAppForRead(*(FPaths::ProjectSavedDir()));
+
+        TArray<FString> Path_Sections;
+        Path_Referance.ParseIntoArray(Path_Sections, TEXT("/"), true);
+        FString Path_Absolute = "/" + Path_Sections[0] + "/" + Path_Sections[1] + "/" + Path_Sections[2] + "/" + In_FileName;
+
+        return Path_Absolute;
     }
 
-    return "";
-}
-
-TArray<uint8> UExtendedVarsBPLibrary::StringToBytesArray(FString In_String)
-{
-    TArray<uint8> Array_Bytes;
-
-    if (In_String.IsEmpty() == true)
+    else
     {
-        return Array_Bytes;
+        return "";
     }
-
-    FTCHARToUTF8 Source = FTCHARToUTF8(In_String.GetCharArray().GetData());
-    Array_Bytes.Append((uint8*)Source.Get(), Source.Length());
-
-    return Array_Bytes;
 }
 
 bool UExtendedVarsBPLibrary::Read_From_Path(UBytesObject_64*& Out_Bytes_Object, FString In_Path)
@@ -73,7 +70,79 @@ bool UExtendedVarsBPLibrary::Read_From_Path(UBytesObject_64*& Out_Bytes_Object, 
     return true;
 }
 
-bool UExtendedVarsBPLibrary::BytesToBytesObject(UBytesObject_64*& Out_Bytes_Object, TArray<uint8> In_Bytes)
+// Bytes Group.
+
+TArray<uint8> UExtendedVarsBPLibrary::String_To_Bytes(FString In_String)
+{
+    TArray<uint8> Array_Bytes;
+
+    if (In_String.IsEmpty() == true)
+    {
+        return Array_Bytes;
+    }
+
+    FTCHARToUTF8 Source = FTCHARToUTF8(In_String.GetCharArray().GetData());
+    Array_Bytes.Append((uint8*)Source.Get(), Source.Length());
+
+    return Array_Bytes;
+}
+
+FString UExtendedVarsBPLibrary::Bytes_To_String(TArray<uint8> In_Bytes)
+{
+    if (In_Bytes.Num() > 0)
+    {
+        const std::string Body_String(reinterpret_cast<const char*>(In_Bytes.GetData()), In_Bytes.Num());
+        FString Body_FString = Body_String.c_str();
+
+        return Body_FString;
+    }
+
+    return "";
+}
+
+TArray<uint8> UExtendedVarsBPLibrary::Base64_To_Bytes(FString In_Base64, bool bUseUrl)
+{
+    TArray<uint8> Array_Bytes;
+
+    if (In_Base64.IsEmpty() == true)
+    {
+        return Array_Bytes;
+    }
+
+    FString Base64_String = In_Base64;
+
+    if (bUseUrl == true)
+    {
+        Base64_String.ReplaceInline(TEXT("-"), TEXT("+"));
+        Base64_String.ReplaceInline(TEXT("_"), TEXT("/"));
+    }
+
+    TArray<uint8> Bytes;
+    FBase64::Decode(Base64_String, Bytes);
+
+    return Bytes;
+}
+
+FString UExtendedVarsBPLibrary::Bytes_To_Base64(TArray<uint8> In_Bytes, bool bUseUrl)
+{
+    if (In_Bytes.Num() == 0)
+    {
+        return "";
+    }
+
+    FString Base64 = FBase64::Encode(In_Bytes.GetData(), In_Bytes.GetAllocatedSize());
+
+    if (bUseUrl)
+    {
+        Base64.ReplaceInline(TEXT("+"), TEXT("-"));
+        Base64.ReplaceInline(TEXT("/"), TEXT("_"));
+        Base64.ReplaceInline(TEXT("="), TEXT(""));
+    }
+
+    return Base64;
+}
+
+bool UExtendedVarsBPLibrary::Bytes_To_B64_OBject(UBytesObject_64*& Out_Bytes_Object, TArray<uint8> In_Bytes)
 {
     if (In_Bytes.Num() == 0)
     {
@@ -91,6 +160,27 @@ bool UExtendedVarsBPLibrary::BytesToBytesObject(UBytesObject_64*& Out_Bytes_Obje
 
     return true;
 }
+
+bool UExtendedVarsBPLibrary::Bytes_To_B32_OBject(UBytesObject_32*& Out_Bytes_Object, TArray<uint8> In_Bytes)
+{
+    if (In_Bytes.Num() == 0)
+    {
+        return false;
+    }
+
+    TArray64<uint8> ByteArray;
+    ByteArray.SetNum(In_Bytes.Num(), true);
+    FMemory::Memcpy(ByteArray.GetData(), In_Bytes.GetData(), In_Bytes.GetAllocatedSize());
+
+    UBytesObject_32* BytesObject_32 = NewObject<UBytesObject_32>();
+    BytesObject_32->ByteArray = ByteArray;
+
+    Out_Bytes_Object = BytesObject_32;
+
+    return true;
+}
+
+// Font Group.
 
 URuntimeFont* UExtendedVarsBPLibrary::RuntimeFont_Load(TArray<uint8> In_Bytes)
 {
@@ -130,7 +220,7 @@ bool UExtendedVarsBPLibrary::RuntimeFont_Release(UPARAM(ref)URuntimeFont*& In_Ru
     return true;
 }
 
-// String Group
+// String Group.
 
 bool UExtendedVarsBPLibrary::Base64ToString(FString In_Base64, FString& OutDecoded)
 {
@@ -170,7 +260,7 @@ TArray<FString> UExtendedVarsBPLibrary::StringSort(TArray<FString> TargetArray, 
     return SortedArray;
 }
 
-// Math Group | Integer
+// Math Group | Integer.
 
 uint8 UExtendedVarsBPLibrary::NumberToByte(int32 In_Number)
 {
@@ -281,12 +371,12 @@ void UExtendedVarsBPLibrary::Int32ToGraphics(EGraphicsType GraphicsType, int32 T
             break;
     }
     
-    Scale = (TargetInteger * FullScale) / FullInteger;
+    Scale = (static_cast<float>(TargetInteger) * FullScale) / FullInteger;
     
     UnitValue = Scale / FullScale;
 }
 
-// Math Group | Float
+// Math Group | Float.
 
 int32 UExtendedVarsBPLibrary::FloatFractionCount(float TargetFloat)
 {
@@ -334,7 +424,7 @@ TArray<float> UExtendedVarsBPLibrary::FloatSort(TArray<float> TargetArray, bool 
     return SortedArray;
 }
 
-// Time Group
+// Time Group.
 
 TArray<FDateTime> UExtendedVarsBPLibrary::TimeSort(TArray<FDateTime> TargetArray, bool bIsDescending)
 {
@@ -359,9 +449,9 @@ TArray<FDateTime> UExtendedVarsBPLibrary::TimeSort(TArray<FDateTime> TargetArray
     return SortedArray;
 }
 
-// Render Group
+// Render Group.
 
-UTextureRenderTarget2D* UExtendedVarsBPLibrary::WidgetToTextureRenderTarget2d(FString& OutCode, UUserWidget* InWidget, FVector2D InDrawSize)
+UTextureRenderTarget2D* UExtendedVarsBPLibrary::Widget_To_TRT2D(FString& OutCode, UUserWidget* InWidget, FVector2D InDrawSize)
 {
     if (IsValid(InWidget) == false)
     {
@@ -387,7 +477,36 @@ UTextureRenderTarget2D* UExtendedVarsBPLibrary::WidgetToTextureRenderTarget2d(FS
     }
 }
 
-bool UExtendedVarsBPLibrary::Texture2dColorsArray(TArray<FColor>& Out_Array, UTexture2D* Texture)
+UTexture2D* UExtendedVarsBPLibrary::TRT2D_To_T2D(UTextureRenderTarget2D* In_TRT_2D)
+{
+    if (IsValid(In_TRT_2D) == false)
+    {
+        return nullptr;
+    }
+
+    FImage Texture_Image;
+    FImageUtils::GetRenderTargetImage(In_TRT_2D, Texture_Image);
+
+    UTexture2D* Texture = UTexture2D::CreateTransient(In_TRT_2D->SizeX, In_TRT_2D->SizeY, PF_B8G8R8A8);
+    FTexture2DMipMap& Texture_Mip = Texture->GetPlatformData()->Mips[0];
+    void* Texture_Data = Texture_Mip.BulkData.Lock(LOCK_READ_WRITE);
+    FMemory::Memcpy(Texture_Data, Texture_Image.RawData.GetData(), Texture_Image.RawData.GetAllocatedSize());
+
+    Texture_Mip.BulkData.Unlock();
+    Texture->UpdateResource();
+
+    if (IsValid(Texture) == true)
+    {
+        return Texture;
+    }
+
+    else
+    {
+        return nullptr;
+    }
+}
+
+bool UExtendedVarsBPLibrary::Export_T2D_Colors(TArray<FColor>& Out_Array, UTexture2D* Texture)
 {
     if (IsValid(Texture) == false)
     {
@@ -421,26 +540,39 @@ bool UExtendedVarsBPLibrary::Texture2dColorsArray(TArray<FColor>& Out_Array, UTe
     }
 }
 
-bool UExtendedVarsBPLibrary::ExportT2dAsBitmap(UTexture2D* Texture, FString Path)
+bool UExtendedVarsBPLibrary::Export_T2D_Bitmap(FString& Out_Path, UTexture2D* Texture, FString In_Path, bool bUseTemp)
 {
     if (IsValid(Texture) == false)
     {
         return false;
     }
 
-    if (Path.IsEmpty() == true)
+    if (bUseTemp == false && In_Path.IsEmpty() == true)
     {
         return false;
     }
 
-    FPaths::MakeStandardFilename(Path);
+    FString FileName;
+
+    if (bUseTemp == true)
+    {
+        FileName = FPaths::ProjectSavedDir() + "Temp/" + Texture->GetFName().ToString() + ".bmp";
+    }
+
+    else
+    {
+        FPaths::MakeStandardFilename(In_Path);
+        FileName = In_Path;
+    }
 
     TArray<FColor> Array_Colors;
-    bool IsArrayCreated = UExtendedVarsBPLibrary::Texture2dColorsArray(Array_Colors, Texture);
+    bool IsArrayCreated = UExtendedVarsBPLibrary::Export_T2D_Colors(Array_Colors, Texture);
 
     if (IsArrayCreated == true)
     {
-        bool IsBitmapCreated = FFileHelper::CreateBitmap(*Path, Texture->GetSizeX(), Texture->GetSizeY(), Array_Colors.GetData(), NULL, &IFileManager::Get(), NULL, true);
+        bool IsBitmapCreated = FFileHelper::CreateBitmap(*FileName, Texture->GetSizeX(), Texture->GetSizeY(), Array_Colors.GetData(), NULL, &IFileManager::Get(), NULL, true);
+        Out_Path = FileName;
+
         return IsBitmapCreated;
     }
 
@@ -450,28 +582,112 @@ bool UExtendedVarsBPLibrary::ExportT2dAsBitmap(UTexture2D* Texture, FString Path
     }
 }
 
-// Path Group
-
-FString UExtendedVarsBPLibrary::Android_Path_Helper(FString InFileName)
+bool UExtendedVarsBPLibrary::Export_T2D_Jpeg(FString& Out_Path, UTexture2D* Texture, FString In_Path, bool bUseTemp)
 {
-    if (UGameplayStatics::GetPlatformName() == "Android" || UGameplayStatics::GetPlatformName() == "IOS")
+    if (IsValid(Texture) == false)
     {
-        if (InFileName.IsEmpty() == true)
-        {
-            return "";
-        }
+        return false;
+    }
 
-        FString Path_Referance = FPlatformFileManager::Get().GetPlatformFile().ConvertToAbsolutePathForExternalAppForRead(*(FPaths::ProjectSavedDir()));
+    if (bUseTemp == false && In_Path.IsEmpty() == true)
+    {
+        return false;
+    }
 
-        TArray<FString> Path_Sections;
-        Path_Referance.ParseIntoArray(Path_Sections, TEXT("/"), true);
-        FString Path_Absolute = "/" + Path_Sections[0] + "/" + Path_Sections[1] + "/" + Path_Sections[2] + "/" + InFileName;
+    FString FileName;
 
-        return Path_Absolute;
+    if (bUseTemp == true)
+    {
+        FileName = FPaths::ProjectSavedDir() + "Temp/" + Texture->GetFName().ToString() + ".jpg";
     }
 
     else
     {
-        return "";
+        FPaths::MakeStandardFilename(In_Path);
+        FileName = In_Path;
     }
+
+    FImage JpegExporter;
+    FImageUtils::GetTexture2DSourceImage(Texture, JpegExporter);
+    bool bIsJpegCreated = FImageUtils::SaveImageByExtension(*FileName, JpegExporter, 0);
+
+    if (bIsJpegCreated == false)
+    {
+        return false;
+    }
+
+    Out_Path = FileName;
+
+    return true;
+}
+
+bool UExtendedVarsBPLibrary::Export_T2D_Bytes_LL(TArray<uint8>& Out_Bytes, UTexture2D* Texture)
+{
+    if (IsValid(Texture) == false)
+    {
+        return false;
+    }
+
+    FTexture2DMipMap& Texture_Mip = Texture->GetPlatformData()->Mips[0];
+    void* Texture_Data = Texture_Mip.BulkData.Lock(LOCK_READ_WRITE);
+    
+    TArray<uint8> Array_Bytes;
+    size_t Lenght = static_cast<size_t>(Texture->GetSizeX() * Texture->GetSizeY() * 4);
+    Array_Bytes.SetNum(Lenght);
+    FMemory::Memcpy(Array_Bytes.GetData(), Texture_Data, Lenght);
+
+    if (Array_Bytes.GetAllocatedSize() == 0)
+    {
+        return false;
+    }
+   
+    Out_Bytes = Array_Bytes;
+
+    Texture_Mip.BulkData.Unlock();
+
+    return true;
+}
+
+bool UExtendedVarsBPLibrary::Import_T2D_Bytes(UTexture2D*& Out_Texture, TArray<uint8> In_Bytes, bool bUseSrgb)
+{
+    if (In_Bytes.Num() == 0)
+    {
+        return false;
+    }
+
+    UTexture2D* Texture = FImageUtils::ImportBufferAsTexture2D(In_Bytes);
+    Texture->SRGB = bUseSrgb;
+
+    if (IsValid(Texture) == true)
+    {
+        Out_Texture = Texture;
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+bool UExtendedVarsBPLibrary::Import_T2D_Bytes_LL(UTexture2D*& Out_Texture, TArray<uint8> In_Bytes, FVector2D Size, bool bUseSrgb)
+{
+    if (In_Bytes.Num() == 0)
+    {
+        return false;
+    }
+
+    UTexture2D* Texture = UTexture2D::CreateTransient(Size.X, Size.Y, PF_B8G8R8A8);
+    Texture->SRGB = bUseSrgb;
+
+    FTexture2DMipMap& Texture_Mip = Texture->GetPlatformData()->Mips[0];
+    void* Texture_Data = Texture_Mip.BulkData.Lock(LOCK_READ_WRITE);
+
+    FMemory::Memcpy(Texture_Data, In_Bytes.GetData(), In_Bytes.Num());
+    Texture_Mip.BulkData.Unlock();
+    Texture->UpdateResource();
+
+    Out_Texture = Texture;
+
+    return true;
 }
--- Source/ExtendedVars/Public/ExtendedVarsBPLibrary.h
+++ Source/ExtendedVars/Public/ExtendedVarsBPLibrary.h
@@ -54,27 +54,43 @@ class UExtendedVarsBPLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_UCLASS_BODY()
 
-	// Bytes Group
-
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Bytes Array To String", Keywords = "http, web, server, api, bind, route, post"), Category = "Extended Variables|Bytes")
-	static EXTENDEDVARS_API FString BytesArrayToString(TArray<uint8> In_Bytes);
-		
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "String to Bytes", Keywords = "string, to, bytes"), Category = "Extended Variables|Bytes")
-	static EXTENDEDVARS_API TArray<uint8> StringToBytesArray(FString In_String);
+	// Path Group.
+	
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Android Path Helper", ToolTip = "This node automatically gives Internal Storage absolute path. So, you just need to define sub-folder and file. Example: Download/sample.pdf", Keywords = "android, ios, mobile, folder, file, path, helper, absolute"), Category = "Extended Variables|Path")
+	static EXTENDEDVARS_API FString Android_Path_Helper(FString In_FileName);
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Read from Path", ToolTip = "You need to use absolute platform path.", Keywords = "read, load, path, bytes"), Category = "Extended Variables|Bytes")
 	static EXTENDEDVARS_API bool Read_From_Path(UBytesObject_64*& Out_Bytes_Object, FString In_Path);
 
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Bytes to Bytes Object", ToolTip = "", Keywords = "read, load, path"), Category = "Extended Variables|Bytes")
-	static EXTENDEDVARS_API bool BytesToBytesObject(UBytesObject_64*& Out_Bytes_Object, TArray<uint8> In_Bytes);
+	// Bytes Group.
 	
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Runtime Font Load", ToolTip = "", Keywords = "load, path, font, runtime"), Category = "Extended Variables | Bytes")
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "String to Bytes", Keywords = "string, bytes"), Category = "Extended Variables|Bytes")
+	static EXTENDEDVARS_API TArray<uint8> String_To_Bytes(FString In_String);
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Bytes To String", Keywords = "http, web, server, api, bind, route, post"), Category = "Extended Variables|Bytes")
+	static EXTENDEDVARS_API FString Bytes_To_String(TArray<uint8> In_Bytes);
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Base64 to Bytes", Keywords = "base64, bytes"), Category = "Extended Variables|Bytes")
+	static EXTENDEDVARS_API TArray<uint8> Base64_To_Bytes(FString In_Base64, bool bUseUrl);
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Bytes To Base64", Keywords = "http, web, server, api, bind, route, post"), Category = "Extended Variables|Bytes")
+	static EXTENDEDVARS_API FString Bytes_To_Base64(TArray<uint8> In_Bytes, bool bUseUrl);
+		
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Bytes to Bytes64 Object", ToolTip = "", Keywords = "read, load, path"), Category = "Extended Variables|Bytes")
+	static EXTENDEDVARS_API bool Bytes_To_B64_OBject(UBytesObject_64*& Out_Bytes_Object, TArray<uint8> In_Bytes);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Bytes to Bytes32 Object", ToolTip = "", Keywords = "read, load, path"), Category = "Extended Variables|Bytes")
+	static EXTENDEDVARS_API bool Bytes_To_B32_OBject(UBytesObject_32*& Out_Bytes_Object, TArray<uint8> In_Bytes);
+
+	// Fonts Group.
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Runtime Font Load", ToolTip = "", Keywords = "runtime, font, load"), Category = "Extended Variables|Font")
 	static EXTENDEDVARS_API URuntimeFont* RuntimeFont_Load(TArray<uint8> In_Bytes);
 
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Runtime Font Load", ToolTip = "", Keywords = "load, path, font, runtime"), Category = "Extended Variables | Bytes")
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Runtime Font Release", ToolTip = "", Keywords = "runtime, font, release"), Category = "Extended Variables|Font")
 	static EXTENDEDVARS_API bool RuntimeFont_Release(UPARAM(ref)URuntimeFont*& In_RuntimeFont);
 
-	// String Group
+	// String Group.
 
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "Base64 URL to String", ToolTip = "Description.", Keywords = "sort, string, fstring, ascending, descending"), Category = "Extended Variables|String")
 	static EXTENDEDVARS_API bool Base64ToString(FString In_Base64, FString& OutDecoded);
@@ -85,7 +101,7 @@ class UExtendedVarsBPLibrary : public UBlueprintFunctionLibrary
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "Sort Strings", ToolTip = "Description.", Keywords = "sort, string, fstring, ascending, descending"), Category = "Extended Variables|String")
 	static EXTENDEDVARS_API TArray<FString> StringSort(TArray<FString> TargetArray, bool bIsDescending);
 
-	// Math Group | Integer
+	// Math Group | Integer.
 
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "Number to Byte", ToolTip = "It converts number to a byte form.", Keywords = "byte, string, fstring, convert"), Category = "Extended Variables|Math")
 	static EXTENDEDVARS_API uint8 NumberToByte(int32 In_Number);
@@ -105,7 +121,7 @@ class UExtendedVarsBPLibrary : public UBlueprintFunctionLibrary
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "Int32 To Graphics", ToolTip = "Unit Value gives scale as a float between 0 and 1.", Keywords = "int32, graphics, pie"), Category = "Extended Variables|Integer")
 	static EXTENDEDVARS_API void Int32ToGraphics(EGraphicsType GraphicsType, int32 TargetInteger, int32 FullInteger, float& Scale, float& UnitValue);
 
-	// Math Group | Float
+	// Math Group | Float.
 
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "Get Fraction Count", ToolTip = "Description.", Keywords = "float, get, fraction, count"), Category = "Extended Variables|Float")
 	static EXTENDEDVARS_API int32 FloatFractionCount(float TargetFloat);
@@ -116,24 +132,35 @@ class UExtendedVarsBPLibrary : public UBlueprintFunctionLibrary
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "Sort Floats", ToolTip = "Description.", Keywords = "sort, float, ascending, descending"), Category = "Extended Variables|Float")
 	static EXTENDEDVARS_API TArray<float> FloatSort(TArray<float> TargetArray, bool bIsDescending);
 	
-	// Time Group
+	// Time Group.
 
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "Sort Times", ToolTip = "Description.", Keywords = "sort, times, ascending, descending"), Category = "Extended Variables|Time")
 	static EXTENDEDVARS_API TArray<FDateTime> TimeSort(TArray<FDateTime> TargetArray, bool bIsDescending);
 
-	// Render Group
+	// Render Group.
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Widget To Texture Render Target 2D", Keywords = "texture, render, target, 2d, widget, convert"), Category = "Extended Variables|Render")
-	static EXTENDEDVARS_API UTextureRenderTarget2D* WidgetToTextureRenderTarget2d(FString& OutCode, UUserWidget* InWidget, FVector2D InDrawSize);
+	static EXTENDEDVARS_API UTextureRenderTarget2D* Widget_To_TRT2D(FString& OutCode, UUserWidget* InWidget, FVector2D InDrawSize);
 
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Colors Array of Texture2D", Keywords = "t2d, texture2d, utexture2d, create, get, export, color, fcolor, array"), Category = "Extended Variables|Render")
-	static EXTENDEDVARS_API bool Texture2dColorsArray(TArray<FColor>&Out_Array, UTexture2D* Texture);
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Texture Render Target 2D to UTexture2D", Keywords = "texture, render, target, 2d, widget, convert"), Category = "Extended Variables|Render")
+	static EXTENDEDVARS_API UTexture2D* TRT2D_To_T2D(UTextureRenderTarget2D* In_TRT_2D);
 
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Export Texture2D As Bitmap", Keywords = "t2d, texture2d, utexture2d, bitmap, export, create"), Category = "Extended Variables|Render")
-	static EXTENDEDVARS_API bool ExportT2dAsBitmap(UTexture2D* Texture, FString Path);
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Export Texture2D As Colors", Keywords = "t2d, texture2d, utexture2d, get, export, color, fcolor, array"), Category = "Extended Variables|Render")
+	static EXTENDEDVARS_API bool Export_T2D_Colors(TArray<FColor>&Out_Array, UTexture2D* Texture);
 
-	// Path Group
-	UFUNCTION(BlueprintPure, meta = (DisplayName = "Android Path Helper", ToolTip = "This node automatically gives Internal Storage absolute path. So, you just need to define sub-folder and file. Example: Download/sample.pdf", Keywords = "android, ios, mobile, folder, file, path, helper, absolute"), Category = "Extended Variables|Path")
-	static EXTENDEDVARS_API FString Android_Path_Helper(FString InFileName);
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Export Texture2D As Bitmap", Keywords = "t2d, texture2d, utexture2d, export, bitmap, bmp"), Category = "Extended Variables|Render")
+	static EXTENDEDVARS_API bool Export_T2D_Bitmap(FString& Out_Path, UTexture2D* Texture, FString In_Path, bool bUseTemp);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Export Texture2D As Jpeg", Keywords = "t2d, texture2d, utexture2d, export, jpeg, jpg"), Category = "Extended Variables|Render")
+	static EXTENDEDVARS_API bool Export_T2D_Jpeg(FString& Out_Path, UTexture2D* Texture, FString In_Path, bool bUseTemp);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Export Texture2D As Bytes Low Level", Keywords = "t2d, texture2d, utexture2d, export, byte, bytes, low, level"), Category = "Extended Variables|Render")
+	static EXTENDEDVARS_API bool Export_T2D_Bytes_LL(TArray<uint8>& Out_Bytes, UTexture2D* Texture);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Import Texture2D From Bytes", Keywords = "t2d, texture2d, utexture2d, import, create, bytes"), Category = "Extended Variables|Render")
+	static EXTENDEDVARS_API bool Import_T2D_Bytes(UTexture2D*& Out_Texture, TArray<uint8> In_Bytes, bool bUseSrgb);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Import Texture2D From Bytes Low Level", Keywords = "t2d, texture2d, utexture2d, import, create, bytes, low, level"), Category = "Extended Variables|Render")
+	static EXTENDEDVARS_API bool Import_T2D_Bytes_LL(UTexture2D*& Out_Texture, TArray<uint8> In_Bytes, FVector2D Size, bool bUseSrgb);
 
 };
